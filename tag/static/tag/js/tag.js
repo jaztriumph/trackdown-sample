b274ddb84c96da47c3faa236e2575a6d84c9833d
Jayanth Reddy <sjayanthreddy2@gmail.com>
2017-12-06 23:27:04 +0530

    collected static files

--- tag/static/tag/js/tag.js
+++ tag/static/tag/js/tag.js
@@ -1,14 +1,15 @@
-    function empty() {
-         var x;
+function empty() {
+    var x;
     x = document.getElementById("id_tagname").value;
-    if (x.trim()=="") {
+    if (x.trim() == "") {
         alert("Tag should not be empty");
         return false;
     }
-    }
-    function copyText() {
-        var copyText = document.getElementById("link");
-        copyText.select();
-        document.execCommand("Copy");
-        document.getSelection().removeAllRanges();
-    }
+}
+
+function copyText() {
+    var copyText = document.getElementById("link");
+    copyText.select();
+    document.execCommand("Copy");
+    document.getSelection().removeAllRanges();
+}
