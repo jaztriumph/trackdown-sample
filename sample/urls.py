a67f457d47358df5456ebb3a91663ce4030718d7
Jayanth Reddy <sjayanthreddy2@gmail.com>
2017-12-07 03:59:56 +0530

    added static root url pattern

--- sample/urls.py
+++ sample/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('tag.urls', namespace='tag'))
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
